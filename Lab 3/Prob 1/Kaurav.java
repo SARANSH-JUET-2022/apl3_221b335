a23134e8e041d0800b24e9320dded9c0bc0d9bd6
221B335 <221B335@juet.co.in>
2025-02-05 15:30:59 +0530

    lab 3

---
+++ Lab 3/Prob 1/Kaurav.java
@@ -0,0 +1,15 @@
+class Kaurav extends Bharatvanshi {
+    @Override
+    public void fight() {
+        System.out.println("Kaurav fighting skills");
+    }
+
+    @Override
+    public void obey() {
+        System.out.println("Kaurav disobeying");
+    }
+
+    public void cruel() {
+        System.out.println("Kaurav showing cruelty");
+    }
+}
---
+++ Lab 3/Prob 1/Vikarn.java
@@ -0,0 +1,10 @@
+class Vikarn extends Kaurav {
+    @Override
+    public void obey() {
+        System.out.println("Vikarn obeying");
+    }
+
+    public void kind() {
+        System.out.println("Vikarn showing kindness");
+    }
+}
