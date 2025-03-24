c354836546796efcd4884f185fc5763ff30fc955
Saransh <221b335@juetguna.in>
2025-03-24 10:40:20 +0530

    beer code added

---
+++ Lab 3/Prob 1/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        Pandav Arjun = new Pandav();
+        Arjun.fight();
+        Arjun.obey();
+        Arjun.kind();
+        
+        Pandav Bheem = new Pandav();
+        Bheem.fight();
+        Bheem.obey();
+        Bheem.kind();
+
+        Kaurav Duryodhan = new Kaurav();
+        Duryodhan.fight();
+        Duryodhan.obey();
+        Duryodhan.cruel();
+
+        Vikarn vikarn = new Vikarn();
+        vikarn.fight();
+        vikarn.obey();
+        vikarn.kind();
+    }
+}
---
+++ Lab 3/Prob 1/Pandav.java
@@ -0,0 +1,15 @@
+class Pandav extends Bharatvanshi {
+    @Override
+    public void fight() {
+        System.out.println("Pandav fighting skills");
+    }
+
+    @Override
+    public void obey() {
+        System.out.println("Pandav obeying");
+    }
+
+    public void kind() {
+        System.out.println("Pandav showing kindness");
+    }
+}
